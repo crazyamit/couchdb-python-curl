2225e8d3e110fd53499a2862af4acef250cc4fcd
elf2001 <elf2001@23b45db2-eff7-11de-a198-37193048d1ed>
2010-11-16 12:15:19 +0000

    classifiers, requirements

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 setup(name='couchdb-python-curl',
       version='1.0',
       description='CouchDB-python wrapper (using cURL library)',
@@ -8,6 +8,15 @@ setup(name='couchdb-python-curl',
       author_email='elf2001@gmail.com',
       url='http://code.google.com/p/couchdb-python-curl/',
       packages=['couchdbcurl'],
-      requires=['pycurl'],
       license='GPL',
+      classifiers=[
+          "Development Status :: 5 - Production/Stable", 
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Natural Language :: English",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Database :: Front-Ends",
+          ],
+      install_requires = ['pycurl']
       )
