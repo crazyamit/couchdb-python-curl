611384e34f5f39b8b662a2021ba97c6d3a40375f
elf2001 <elf2001@23b45db2-eff7-11de-a198-37193048d1ed>
2011-01-18 10:22:15 +0000

    permanent view() call with keys argument must has application/json content-type header

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 setup(name='couchdb-python-curl',
-      version='1.0.0',
+      version='1.0.1',
       description='CouchDB-python wrapper (using cURL library)',
       author='Alexey Loshkarev',
       author_email='elf2001@gmail.com',
